04042916c3a24381271db94de99212c5d3e70ec8
Supriya2007 <supriyakavi2016@gmail.com>
2021-08-04 19:53:21 +0530

    Comments

--- flask-app/lexer_parser.py
+++ flask-app/lexer_parser.py
@@ -1,5 +1,6 @@
 """Lexer and parser module for pseudo-code"""
 # pylint: disable=invalid-name,unused-argument,global-statement
+# >, >=, <, <=, =, !=
 import ply.lex as lex
 import ply.yacc as yacc
 import yaml
@@ -333,6 +334,8 @@ def p_value_expr(p):
         python_code = convert_pseudocode_to_python("NUMBER", value = p[1])
     p[0] = python_code
 
+#obstacle dir - ahead, behind, left, right
+#begin and end
 def p_selection_expr(p):
     '''
     selection_expr : IFOBSTACLEAHEAD BEGIN exprs END
